75f7ab99b4fc9d3fdea5765e1bb40149e32105f8
Snehal Jain <jainsnehal9946@gmail.com>
2026-07-18 19:00:35 +0530

    2pointers pattern: 2sum

--- two_sum_2P.java
+++ two_sum_2P.java
@@ -1,30 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // Store elements with their original indices
-        int[][] numWithIndex = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            numWithIndex[i][0] = nums[i];
-            numWithIndex[i][1] = i;
-        }
-
-        // Sort the array based on values for the two-pointer approach
-        Arrays.sort(numWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left < right) {
-            int sum = numWithIndex[left][0] + numWithIndex[right][0];
-            if (sum == target) {
-                return new int[]{numWithIndex[left][1], numWithIndex[right][1]};
-            } else if (sum < target) {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        int right=numbers.length-1;
+        int a[]=new int[2];
+        a[0]=0;
+        a[1]=0;
+        while(left<right)
+        {
+            if(numbers[left]+numbers[right]<target)
+            {
                 left++;
-            } else {
+            }else if(numbers[left]+numbers[right]>target){
                 right--;
+            }else if(numbers[left]+numbers[right]==target)
+            {
+                a[0]=left+1;
+                a[1]=right+1;
+                break;
+
             }
         }
-        return new int[]{};
+        return a;
+        
     }
 }
